b64851f39794b11c4d0438fa964e62f373ceb28e
ryeongse25 <seryeong971120@gmail.com>
2023-05-17 21:21:47 +0900

    refactor: extract main page background img to Map component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
+import Map from "@/components/main/Map";
+
 export default function Home() {
   return (
     <>
-      <div className="main d-flex justify-content-center align-items-center">
-        <img src="/img/main/map.png" width="480px" />
-      </div>
-      <style jsx>{`
-        .main {
-          height: 100vh;
-          overflow-y: hidden;
-          background: #50c878;
-          padding-top: var(--main-header-height);
-        }
-      `}</style>
+      <Map />
     </>
   );
 }
