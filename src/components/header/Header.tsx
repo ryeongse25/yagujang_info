e2a21fdee85a7d2367925a22f348917313c00578
ryeongse25 <seryeong971120@gmail.com>
2023-05-18 21:01:28 +0900

    style: nav container UI

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,12 +2,16 @@ export default function Header() {
   return (
     <>
       <header>
-        <div className="container"></div>
+        <div className="container">
+          <nav></nav>
+        </div>
       </header>
       <style jsx>{`
         header {
           position: absolute;
           width: 100vw;
+        }
+        header nav {
           height: var(--main-header-height);
           border: 2px dashed white;
         }
